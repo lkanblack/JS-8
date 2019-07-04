4a8f79a1b6ab2ca5c0456d5c2730eea63d669441
Andrei Dubinko <32032745+lkanblack@users.noreply.github.com>
2019-07-04 11:58:09 +0300

    усложненное задание 4

--- lesson04/js/hard.js
+++ lesson04/js/hard.js
@@ -1,2 +1,10 @@
 'use strict';
 
+function meaning(mean){
+    if(typeof mean != 'string'){
+        alert('Not a string!');
+    } else {
+        console.log(mean.trim().substring(0, 30)+ '...');
+    }
+}
+  meaning('      Need to clean all spaces Needto clean all spacesNeed to clean all spaces');
