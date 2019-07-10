41f4cec79fc8a39aa509a5c410e5a360006ba77b
Andrei Dubinko <andrei.dubinko@gmail.com>
2019-07-10 21:07:11 +0300

    начал усложненное задание номер 5

--- lesson05/js/hard.js
+++ lesson05/js/hard.js
@@ -1,2 +1,19 @@
 'use strict';
 
+let arr = ['432','202','808','43434','55232','871','276'];
+
+arr.forEach(function(item) {
+    if (item.startsWith('4') || item.startsWith('2')) {
+      console.log(item);
+    }
+  });
+
+nextPrime:
+  for (var i = 2; i <= 100; i++) {
+
+    for (var j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime ;
+    } 
+
+    console.log( i );
+  }
