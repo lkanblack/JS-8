6a174e8c47892c763dfb7763395c9fb664604dcb
Andrei Dubinko <andrei.dubinko@gmail.com>
2019-07-17 20:14:24 +0300

    параграф выводит текст , лишние console.log удалены

--- lesson10/script.js
+++ lesson10/script.js
@@ -14,15 +14,15 @@ DomElement.prototype.create = function(){
         newDiv.classList.add(this.selector.substr(1));
         newDiv.textContent = "Some text";
         newDiv.style.cssText=`height:${this.height};width:${this.width};background:${this.bg};font-size:${this.fontSize}`
-        console.log(newDiv);
         document.body.appendChild(newDiv);
     } else if(this.selector[0] === '#') {
         let newPar = document.createElement('p');
-        console.log(newPar);
         document.body.appendChild(newPar);
-
+        newPar.textContent = "Some text";
     }
 }
 
 let domElement = new DomElement('.block', '100px', '100px', 'red', '20px');
 domElement.create();
+let domElement2 = new DomElement('#block', '100px', '100px', 'red', '20px');
+domElement2.create();
