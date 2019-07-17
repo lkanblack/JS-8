ad95e9b110ec1168acace43d8df2f34219a31d71
Andrei Dubinko <32032745+lkanblack@users.noreply.github.com>
2019-07-17 15:07:52 +0300

    Add files via upload

--- lesson06/js/hard.js
+++ lesson06/js/hard.js
@@ -1,13 +1,12 @@
 'use strict';
+let week = ['понедельник', 'вторник', 'среда','четверг','пятница',`<i>суббота</i>`,`<i>воскресенье</i>`];
+var date = new Date().getDay() -1;
 
-let week = ['понедельник', 'вторник', 'среда','четверг','пятница',`<i>суббота</i>`,`<i>воскресенье</i>`],
-today = new Date().getDay() -1;
 
-for (let i = 0; i < week.length; i++) {
-if (i == today) {
-    document.write(`<p><b>${week[i]}</b></p>`);
-}  else {
-  document.write(`<p>${week[i]}</p>`);
+for(let i = 0; i < week.length; i++){
+    if(i == date){
+        document.write(`<b>${week[i]}</b>`);
+    } else {
+        document.write(`<p>${week[i]}</p>`)
+    }
 }
-}
-console.log(week);
