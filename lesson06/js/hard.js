3d43ddf51043eb9ebd9eeffce8237a326c1015b2
Andrei Dubinko <32032745+lkanblack@users.noreply.github.com>
2019-07-17 14:51:08 +0300

    Усложненное задание 6

--- lesson06/js/hard.js
+++ lesson06/js/hard.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let week = ['понедельник', 'вторник', 'среда','четверг','пятница',`<i>суббота</i>`,`<i>воскресенье</i>`],
+today = new Date().getDay() -1;
+
+for (let i = 0; i < week.length; i++) {
+if (i == today) {
+    document.write(`<p><b>${week[i]}</b></p>`);
+}  else {
+  document.write(`<p>${week[i]}</p>`);
+}
+}
+console.log(week);
