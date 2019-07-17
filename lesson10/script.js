47f0b983d0fb94e61e3fe44d44b17350dc661103
Andrei Dubinko <32032745+lkanblack@users.noreply.github.com>
2019-07-17 11:58:22 +0300

    ДЗ 10

--- lesson10/script.js
+++ lesson10/script.js
@@ -12,17 +12,17 @@ DomElement.prototype.create = function(){
     if(this.selector[0] === '.') {
         let newDiv = document.createElement('div');
         newDiv.classList.add(this.selector.substr(1));
-        newDiv.style.cssText= "height: $this.height;";
+        newDiv.textContent = "Some text";
+        newDiv.style.cssText=`height:${this.height};width:${this.width};background:${this.bg};font-size:${this.fontSize}`
         console.log(newDiv);
         document.body.appendChild(newDiv);
     } else if(this.selector[0] === '#') {
         let newPar = document.createElement('p');
-        newPar.classList.add(this.selector.substr(1));
         console.log(newPar);
         document.body.appendChild(newPar);
 
     }
 }
 
-let domElement = new DomElement('.block', '100px', '30px', 'red', '20px');
+let domElement = new DomElement('.block', '100px', '100px', 'red', '20px');
 domElement.create();
