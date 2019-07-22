2915a7babe3c3f6b0aeb7e0dfc36afc6cbaa0167
Andrei Dubinko <32032745+lkanblack@users.noreply.github.com>
2019-07-22 14:03:29 +0300

    Исправил усложненное ДЗ 6

--- lesson06/js/hard.js
+++ lesson06/js/hard.js
@@ -1,12 +1,16 @@
 'use strict';
-let week = ['понедельник', 'вторник', 'среда','четверг','пятница',`<i>суббота</i>`,`<i>воскресенье</i>`];
+let week = ['понедельник', 'вторник', 'среда','четверг','пятница','суббота','воскресенье'];
 var date = new Date().getDay() -1;
+let block = document.querySelector('.block');
 
 
 for(let i = 0; i < week.length; i++){
+    let element = document.createElement('p');
+    block.appendChild(element);
+    element.textContent = week[i];
     if(i == date){
-        document.write(`<b>${week[i]}</b>`);
-    } else {
-        document.write(`<p>${week[i]}</p>`)
+        element.style.fontWeight = '900';
+    } else if(week[i] == 'суббота' || week[i] == 'воскресенье') {
+        element.style.fontStyle = 'italic';
     }
 }
